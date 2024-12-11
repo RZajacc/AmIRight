6e094d32f0462e32a92b6f6398958239e2b06ea6
RZajacc <rf.zajac@gmail.com>
2024-12-11 12:55:29 +0100

    created an initial structure for a desktop navigation

--- src/components/header/desktopNav/destktopNav.tsx
+++ src/components/header/desktopNav/destktopNav.tsx
@@ -1,9 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
 
 function DestktopNav({}: Props) {
-  return <div>destktopNav</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href={"/"}>Am I right?</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Current issues</Link>
+        </li>
+        <li>
+          <Link href={"/"}>Current Polls</Link>
+        </li>
+        <li>
+          <Link href={"/"}>User</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default DestktopNav;

fecfb5fed988c39f4072e55b7ced15f80b9265ac
RZajacc <rf.zajac@gmail.com>
2024-12-11 13:43:47 +0100

    finished some basic styling for the desktop nav

--- src/components/header/desktopNav/destktopNav.tsx
+++ src/components/header/desktopNav/destktopNav.tsx
@@ -1,24 +1,29 @@
 import Link from "next/link";
 import React from "react";
+import styles from "./desktopNav.module.css";
 
 type Props = {};
 
 function DestktopNav({}: Props) {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href={"/"}>Am I right?</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Current issues</Link>
-        </li>
-        <li>
-          <Link href={"/"}>Current Polls</Link>
-        </li>
-        <li>
-          <Link href={"/"}>User</Link>
-        </li>
+    <nav className={styles.navWrapper}>
+      <ul className={styles.navMain}>
+        <div className={styles.navMain__section}>
+          <li>
+            <Link href={"/"}>Am I right?</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Current issues</Link>
+          </li>
+          <li>
+            <Link href={"/"}>Current Polls</Link>
+          </li>
+        </div>
+        <div className={styles.navMain__section}>
+          <li>
+            <Link href={"/"}>User</Link>
+          </li>
+        </div>
       </ul>
     </nav>
   );

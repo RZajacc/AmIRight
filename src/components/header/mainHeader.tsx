4d80a296446891d63c1c1f56252b30c7a1306f5d
RZajacc <rf.zajac@gmail.com>
2024-12-12 14:14:56 +0100

    changed the idea for navbar, moved all logic into one header component

--- src/components/header/mainHeader.tsx
+++ src/components/header/mainHeader.tsx
@@ -1,16 +1,40 @@
 import React from "react";
-import MobileNav from "./mobileNav/mobileNav";
-import DestktopNav from "./desktopNav/destktopNav";
+import styles from "./mainHeader.module.css";
+import NavLink from "../ui/links/navLink";
+import HamburgerButton from "../ui/buttons/hamburger/hamburgerButton";
 
 type Props = {};
 
 function MainHeader({}: Props) {
   return (
     <header>
-      {/* Mobile navigation */}
-      <MobileNav />
-      {/* Desktop navigation */}
-      <DestktopNav />
+      <nav className={styles.navWrapper}>
+        <ul className={styles.navMain}>
+          <div className={styles.navMain__section}>
+            <li>
+              <NavLink href="/">
+                <span className={styles.home_link}>Am I right?</span>
+              </NavLink>
+            </li>
+          </div>
+          <div className={styles.navMain__section}>
+            <li>
+              <NavLink href="/issues">Current issues</NavLink>
+            </li>
+            <li>
+              <NavLink href="/polls">Polls</NavLink>
+            </li>
+            <li>
+              <NavLink href="/user" about="Get started">
+                Get Started
+              </NavLink>
+            </li>
+            <li>
+              <HamburgerButton />
+            </li>
+          </div>
+        </ul>
+      </nav>
     </header>
   );
 }

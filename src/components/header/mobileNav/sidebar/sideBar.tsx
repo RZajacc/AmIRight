f330493d24445ad924dd6e295ea84c47d60ca52f
RZajacc <rf.zajac@gmail.com>
2024-12-19 14:42:51 +0100

    added new background to sidebar, and button to close it

--- src/components/header/mobileNav/sidebar/sideBar.tsx
+++ src/components/header/mobileNav/sidebar/sideBar.tsx
@@ -1,5 +1,7 @@
 import React, { SetStateAction } from "react";
 import styles from "./sidebar.module.css";
+import cancelButton from "@/assets/cancel-svgrepo-com.svg";
+import Image from "next/image";
 
 type Props = {
   visible: boolean;
@@ -10,6 +12,14 @@ export default function SideBar({ visible, setVisible }: Props) {
   return (
     <>
       <ul className={`${styles.sidebar} ${visible ? styles.visible : ""}`}>
+        <button
+          className={styles.cancelButton}
+          onClick={() => {
+            setVisible((prevVal) => !prevVal);
+          }}
+        >
+          <Image src={cancelButton} width={35} alt="Cancel button" />
+        </button>
         <li>Home</li>
         <li>Test</li>
       </ul>

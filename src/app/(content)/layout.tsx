37ef9793bc309495cba0fa68c8d2411f5dbcb331
RZajacc <rf.zajac@gmail.com>
2024-12-20 21:42:04 +0100

    created a route groups for main greeting page and for all other content

--- src/app/(content)/layout.tsx
+++ src/app/(content)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import MainHeader from "@/components/header/mainHeader";
 
 const geistSans = Geist({

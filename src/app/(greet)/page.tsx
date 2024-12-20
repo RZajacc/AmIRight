1bb791337954713a7a8b5d7fd4b94059bb66e1c5
RZajacc <rf.zajac@gmail.com>
2024-12-20 21:54:37 +0100

    Added two navlinks into the greeting page to check if route groups work. They do

--- src/app/(greet)/page.tsx
+++ src/app/(greet)/page.tsx
@@ -1,7 +1,12 @@
+import NavLink from "@/components/ui/links/navLink";
+
 export default function Home() {
   return (
     <div>
-      <main>xsd</main>
+      <main>
+        <NavLink href="issues">Current issues</NavLink>
+        <NavLink href="user">Get started</NavLink>
+      </main>
     </div>
   );
 }

23c22aee3ce63d9acd07aab0f6831109f5fc465f
RZajacc <rf.zajac@gmail.com>
2024-12-23 14:26:36 +0100

    added home page with some initial styling

--- src/app/(greet)/page.tsx
+++ src/app/(greet)/page.tsx
@@ -1,12 +1,27 @@
 import NavLink from "@/components/ui/links/navLink";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <NavLink href="issues">Current issues</NavLink>
-        <NavLink href="user">Get started</NavLink>
-      </main>
+    <div className={styles.mainContainer}>
+      <h2>Am I right?</h2>
+      <p>
+        For anyone who whatched "How I met your mother" serie, probably you
+        remember the episode where Ted and Barney walk around the city with a
+        bunch of students, and use them to judge who is right on several
+        different subjects. Well this is the implementation of this idea. You
+        can post a question/problem that you encountered with your friend and
+        partner and leave it open for a public to judge if you're right, or
+        wrong about it.
+      </p>
+      <section>
+        <NavLink href="issues" className={styles.navLink}>
+          Current issues
+        </NavLink>
+        <NavLink href="user" className={styles.navLink}>
+          Get started
+        </NavLink>
+      </section>
     </div>
   );
 }

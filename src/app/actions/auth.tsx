babd5ac1526deb8148b0501c8d6cc511810f28f1
RZajacc <rf.zajac@gmail.com>
2025-01-09 16:56:55 +0100

    implememnted validation without matching passwords into signup auth action

--- src/app/actions/auth.tsx
+++ src/app/actions/auth.tsx
@@ -1,1 +1,15 @@
-export async function signup(formData: FormData) {}
+import { FormState, SignupFormSchema } from "../lib/definitions";
+
+export async function signup(state: FormState, formData: FormData) {
+  const validatedFields = SignupFormSchema.safeParse({
+    name: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+}

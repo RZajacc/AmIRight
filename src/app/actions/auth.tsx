f6c09fe3d47955fee530c39304556ef282be6ff3
RZajacc <rf.zajac@gmail.com>
2025-01-17 10:47:38 +0100

    turned auth action to server method to make env variable available to it. Connected with a db and saved first user. So far without psw encryption

--- src/app/actions/auth.tsx
+++ src/app/actions/auth.tsx
@@ -1,8 +1,10 @@
+"use server";
 import { FormState, SignupFormSchema } from "@/lib/definitions";
 import dbConnect from "@/lib/dbConnect";
 import User from "@/models/User";
 
 export async function signup(state: FormState, formData: FormData) {
+  // Validate form data with Zod
   const validatedFields = SignupFormSchema.safeParse({
     username: formData.get("username"),
     email: formData.get("email"),
@@ -10,12 +12,22 @@ export async function signup(state: FormState, formData: FormData) {
     confirm: formData.get("confirm"),
   });
 
+  // If validation fail return errors
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
     };
   }
-  // await dbConnect();
-  // If succeded
-  // await User.create(validatedFields.data);
+
+  // Connect to the DB
+  await dbConnect();
+
+  // Encrypt the password
+
+  // Create a user in the DB
+  await User.create({
+    name: validatedFields.data.username,
+    email: validatedFields.data.email,
+    password: validatedFields.data.password,
+  });
 }

1eea4a1c74c18dd15ccceae02f56b21165cc48c7
Rafal Zajac <rf.zajac@gmail.com>
2025-02-10 17:07:18 +0100

    added route protection to polls page

--- src/app/(content)/polls/page.tsx
+++ src/app/(content)/polls/page.tsx
@@ -1,7 +1,13 @@
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 async function Polls() {
+  // Get user session
   const session = await getServerSession();
+  // If session is not available then redirect the user
+  if (!session?.user) {
+    redirect("/user");
+  }
 
   return <main>Polls</main>;
 }

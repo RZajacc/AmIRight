da7c636d00c50bc56c1266237f4277d6c22e4d7f
Rafal Zajac <rf.zajac@gmail.com>
2025-02-11 18:17:09 +0100

    commented out route protection from polls page

--- src/app/(content)/polls/page.tsx
+++ src/app/(content)/polls/page.tsx
@@ -3,11 +3,11 @@ import { redirect } from "next/navigation";
 
 async function Polls() {
   // Get user session
-  const session = await getServerSession();
+  // const session = await getServerSession();
   // If session is not available then redirect the user
-  if (!session?.user) {
-    redirect("/user");
-  }
+  // if (!session?.user) {
+  //   redirect("/user");
+  // }
 
   return <main>Polls</main>;
 }

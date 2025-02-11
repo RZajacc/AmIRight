569d36d990e59d9847cb5813fb126e1cbd913d6c
Rafal Zajac <rf.zajac@gmail.com>
2025-02-11 18:10:07 +0100

    added useEffect to account page to ensure that the component is loaded before redirection starts

--- src/app/(content)/user/account/page.tsx
+++ src/app/(content)/user/account/page.tsx
@@ -1,7 +1,28 @@
+"use client";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 function page() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  // Redirect after rendering the component
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/user");
+    }
+  }, [status, router]);
+
+  //   Handle logging out
+  const handleLogout = async () => {
+    await signOut();
+    router.push("/user");
+  };
   return (
     <main>
       <div>User account</div>
+      <button onClick={handleLogout}>Logout</button>
     </main>
   );
 }
